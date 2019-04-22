96bcffd239706c765f02348f2edd09e897ebcc45
denis1517 <43105134+denis1517@users.noreply.github.com>
2019-04-22 11:58:19 +0800

    Commit Pertama

---
+++ resources/views/products/edit.blade.php
@@ -0,0 +1,52 @@
+@extends('products.layout')
+   
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <br>    
+                <h2>Edit Product</h2>
+            </div>
+            <div class="pull-right">
+                <br><br><br>
+                <a class="btn btn-primary" href="{{ route('products.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+   
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+  
+    <form action="{{ route('products.update',$product->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+   
+         <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <br>
+                <div class="form-group">
+                    <strong>Nama Produk:</strong>
+                    <input type="text" name="nama_produk" value="{{ $product->nama_produk }}" class="form-control" placeholder="Name">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Harga:</strong>
+                    <input class="form-control" name="harga" placeholder="Detail" value="{{ $product->harga }}"></input>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+   
+    </form>
+@endsection
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,55 @@
+@extends('products.layout')
+ 
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <br>
+                <h2>Data Produk</h2>
+                <br>
+            </div>
+            <div class="pull-right">
+                <br><br><br>
+                <a class="btn btn-success" href="{{ route('products.create') }}"> Create New Product</a>
+            </div>
+        </div>
+    </div>
+   
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+   
+    <table class="table table-bordered">
+        <tr>
+            <th>No</th>
+            <th>Nama Produk</th>
+            <th>Harga</th>
+            <th width="280px">Action</th>
+        </tr>
+        @foreach ($products as $product)
+        <tr>
+            <td>{{ ++$i }}</td>
+            <td>{{ $product->nama_produk }}</td>
+            <td>{{ $product->harga }}</td>
+            <td>
+                <form action="{{ route('products.destroy',$product->id) }}" method="POST">
+   
+                    <a class="btn btn-info" href="{{ route('products.show',$product->id) }}">Show</a>
+    
+                    <a class="btn btn-primary" href="{{ route('products.edit',$product->id) }}">Edit</a>
+   
+                    @csrf
+                    @method('DELETE')
+      
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+  
+    {!! $products->links() !!}
+      
+@endsection
